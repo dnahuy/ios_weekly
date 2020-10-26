942f252a1f83ceb4511155d899a0270529a575c7
Du Nguyen Anh huy <anhhuy.du@foody.vn>
2020-10-26 11:57:27 +0700

    [Weekly_005] Add resources

---
+++ Weekly/005_2020_10_17/How_to_make_XCFramework/Demo/test_xcframework/test_xcframework/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  test_xcframework
+//
+//  Created by Huy Du on 10/19/20.
+//
+
+import Foundation
+
+public struct HuyDemoFramework {
+    public var param1: Int
+    public var param2: Int
+    
+    public init() {
+        self.param1 = 0
+        self.param2 = 0
+    }
+}
---
+++ Weekly/005_2020_10_17/How_to_make_XCFramework/Demo/using_framework/using_framework/test_xcframework.xcframework/ios-arm64_x86_64-simulator/test_xcframework.framework/Headers/test_xcframework.h
@@ -0,0 +1,18 @@
+//
+//  test_xcframework.h
+//  test_xcframework
+//
+//  Created by Huy Du on 10/19/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test_xcframework.
+FOUNDATION_EXPORT double test_xcframeworkVersionNumber;
+
+//! Project version string for test_xcframework.
+FOUNDATION_EXPORT const unsigned char test_xcframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test_xcframework/PublicHeader.h>
+
+
